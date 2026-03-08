5275920826b773db15e3e8c4c4b74fa9c5f5d31c
DemianVGLl2 <0253139@up.edu.mx>
2026-03-08 14:55:55 -0600

    Server: Agregar forma de usar carrito en handleConn para que Sophia parsee comandos del cliente y así.

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -8,12 +8,15 @@ import (
 	"strings"
 
 	"github.com/DemianVGLl2/SuperMercadoUP/internal/logger"
+	"github.com/DemianVGLl2/SuperMercadoUP/internal/models"
 )
 
 func main() {
-	if err := logger.Init("server.txt"); err != nil {
+	if err := logger.Init("server.log"); err != nil {
 		log.Fatal("Error iniciando logger: ", err)
 	}
+
+	store := models.NewStore()
 
 	listener, err := net.Listen("tcp", "localhost:8000")
 
@@ -31,14 +34,17 @@ func main() {
 			continue
 		}
 
-		go handleConn(conn)
+		go handleConn(conn, store)
 	}
 }
 
-func handleConn(conn net.Conn) {
+func handleConn(conn net.Conn, store *models.Store) {
 	defer conn.Close()
 	addr := conn.RemoteAddr().String()
 	logger.Log("SERVER", "CONNECT", addr)
+
+	cart := models.Cart{}
+	_ = cart // esto solo es para evitar errores
 
 	input := bufio.NewScanner(conn)
 	for input.Scan() {
@@ -46,6 +52,8 @@ func handleConn(conn net.Conn) {
 		if line == "" {
 			continue
 		}
+
+		// Aquí Sophía puede hacer todo lo de los comandos con el carrito :D
 
 		fmt.Fprintf(conn, "ECHO: %s\n", line)
 	}
